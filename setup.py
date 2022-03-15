1296fb34358df285155b45e4db5d262293604da6
masadcv <muhammad.asad@kcl.ac.uk>
2022-03-15 03:01:30 +0000

    add long description type
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(name='torchpropel',
       version='0.0.2',
       description='Probabilistic Parameteric Regression Loss (PROPEL)',
       long_description=readme(),
+      long_description_content_type="text/markdown",
       classifiers=[
         'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
